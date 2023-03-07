07eef03fbd89f48d628d5201171a2ad38768db54
Ilya Krukov <ujfox13@gmail.com>
2023-03-07 15:11:38 +0300

    Layout

--- src/Layout/index.tsx
+++ src/Layout/index.tsx
@@ -1,10 +1,29 @@
-import { ReactNode } from "react";
+import { Grid, GridItem } from "@chakra-ui/react";
+import Header from "components/Header";
+import NavBar from "components/NavBar";
+import { ReactNode, useState } from "react";
 
 const Layout = ({ children }: { children: ReactNode }) => {
+
     return (
-        <div>
-            {children}
-        </div>
+        <Grid
+            h="100vh"
+            templateRows={"100px 1fr"}
+            templateColumns={"50px 1fr"}
+
+            templateAreas={`"header header"
+                            "nav    main"`}
+        >
+            <GridItem area={"header"}>
+                <Header />
+            </GridItem>
+            <GridItem area={"nav"}>
+                <NavBar/>
+            </GridItem>
+            <GridItem area="main">
+                {children}
+            </GridItem>
+        </Grid>
     );
 };
 
