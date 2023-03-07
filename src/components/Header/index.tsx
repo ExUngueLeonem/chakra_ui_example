3a05e8bffd60783c55092de1ad48bc5fd5f7a587
Ilya Krukov <ujfox13@gmail.com>
2023-03-07 15:12:13 +0300

    header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+import { Box } from "@chakra-ui/react";
+
+const Header = () => {
+    return (
+        <Box bgColor={"green.500"} h="100%">
+            Header
+        </Box>
+    );
+};
+
+export default Header;
