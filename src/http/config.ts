f180b19b78552b551c77018d0854efe557f43112
Ilya Krukov <ujfox13@gmail.com>
2023-03-07 17:54:16 +0300

    json-server

--- src/http/config.ts
+++ src/http/config.ts
@@ -1,3 +1,5 @@
 export const apiConfig = {
-    baseUrl: "https://api.storyblok.com/v1/cdn/stories/demo?version=published&token=8jpUYBYwD9LKFSJXQKx7DQtt&cv=1566469459" 
+    // baseUrl: "https://api.storyblok.com/v1/cdn/stories/demo?version=published&token=8jpUYBYwD9LKFSJXQKx7DQtt&cv=1566469459",
+    //http://localhost:3000/story
+    baseUrl: "http://localhost:3000/story", 
 }
