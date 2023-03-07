1dd5e9963a152249be28036e21e02f4e7565af3b
Ilya Krukov <ujfox13@gmail.com>
2023-03-07 18:10:37 +0300

    [wip]

--- src/pages/ApiFromServer.tsx
+++ src/pages/ApiFromServer.tsx
@@ -2,6 +2,29 @@ import { MainApi } from "http/MainApi";
 import { useEffect, useState } from "react";
 import { IApiData } from "types";
 
+const Foo = ({ block }: { block: string }) => {
+    return (
+        <div>
+            Foo
+            {block}
+        </div>
+    );
+};
+
+const Bar = ({ block }: { block: string }) => {
+    return (
+        <div>
+            Bar
+            {block}
+        </div>
+    );
+};
+
+const Components = {
+    foo: Foo,
+    bar: Bar,
+}
+
 const ApiFromServer = () => {
     const [data, setData] = useState<IApiData>();
     useEffect(() => {
@@ -12,12 +35,29 @@ const ApiFromServer = () => {
 
     return (
         <div>
-            {/* {data &&
-                JSON.stringify(data)
-            } */}
-            ApiFromServer
+            {data &&
+                data.content.body.map(block => block.component)
+            }
         </div>
     );
 };
 
 export default ApiFromServer;
+
+//https://codesandbox.io/s/dynamic-components-from-json-with-react-ybq27?fontsize=14&file=/src/components.js:174-488
+const GetComponents = ({ block }: { block: any }) => {
+    if (typeof Components[block.component] !== "undefined") {
+        return React.createElement(Components[block.component], {
+          key: block._uid,
+          block: block
+        });
+      }
+      return React.createElement(
+        () => <div>The component {block.component} has not been created yet.</div>,
+        { key: block._uid }
+      );
+};
+
+
+
+
