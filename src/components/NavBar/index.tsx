768adbf8926ef8509f17bff579c074fcab8f9446
Ilya Krukov <ujfox13@gmail.com>
2023-03-07 16:52:09 +0300

    navbar

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,18 +1,19 @@
-import { ArrowBackIcon, ArrowForwardIcon, CloseIcon } from "@chakra-ui/icons";
-import { Box, IconButton } from "@chakra-ui/react";
-import { useState } from "react";
+import { ArrowBackIcon, ArrowForwardIcon } from "@chakra-ui/icons";
+import { Box, IconButton, useBoolean, useColorModeValue } from "@chakra-ui/react";
+import { palette } from "styles/themeStyle";
 
 const NavBar = () => {
-    const [isNavbarOpen, setIsNavbarOpen] = useState(false);
-    const toggleNavbar = () => setIsNavbarOpen(!isNavbarOpen)
+    const [isOpen, setIsOpen] = useBoolean(false);
+    const themeText = useColorModeValue("light", "dark")
 
+    console.log("text", themeText)
     return (
-        <Box h="100%" bgColor={"gray.200"} w={isNavbarOpen ? "360px" : "50px"}>
+        <Box h="100%" bgColor={palette[themeText].bgGrey} w={isOpen ? "360px" : "50px"}>
             <Box p="1">
-                <IconButton aria-label="close-navbar" variant={"outline"} icon={isNavbarOpen ? <ArrowBackIcon /> : <ArrowForwardIcon />} onClick={toggleNavbar} />
+                <IconButton aria-label="close-navbar" variant={"outline"} icon={isOpen ? <ArrowBackIcon /> : <ArrowForwardIcon />} onClick={setIsOpen.toggle} />
             </Box>
             <Box>
-                {isNavbarOpen ?
+                {isOpen ?
                     "строки"
                     :
                     "Иконки"
